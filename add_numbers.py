e927815a9db687efc74cd74929dfe34544bdbb34
sujatabande27 <sujatabande27@gmail.com>
2026-04-09 18:56:54 +0530

    Comlete code

--- add_numbers.py
+++ add_numbers.py
@@ -1,2 +1,6 @@
-print("My first leetcode solution")
-print("Thank you!!!!")
+class Solution(object):
+    def twoSum(self, nums, target):
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
