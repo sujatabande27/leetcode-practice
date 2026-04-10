a1c09530a0b6f702242fc817a986b5b2d881e668
sujatabande27 <sujatabande27@gmail.com>
2026-04-10 12:01:50 +0530

    New approach

--- add_numbers.py
+++ add_numbers.py
@@ -1,6 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+       hash={}
+       for i in range(len(nums)):
+        complement=target-nums[i]
+        if complement in hash:
+            return [hash[complement],i]
+        hash[nums[i]]=i
